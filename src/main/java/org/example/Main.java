b53d6f297cdd6ed6b69e5d8b27d18d55bad6c867
rrohyo <aoslwjs2926@gmail.com>
2025-03-17 21:24:08 +0900

    git setting 출력문 추가

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요 \n "+
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" +
+                "안녕하세요 \n" );
+        System.out.println("고쪽이");
+        System.out.println("고쪽이");
+        System.out.println("고쪽이");
+        System.out.println("고쪽이");
+        System.out.println("배그");
+
+
+
+    }
+    }
